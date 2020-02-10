2022faae523f9d6d2f25d00cbd6d6205cfdfdec2
ZhangDianxiong <zhangdi@oregonstate.edu>
2020-02-10 08:32:02 -0800

    done the clander part, invoice is half way done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./login.css";
 import Login from "./login";
 import Home from "./home";
 import Registration from "./registration";
-import ProjectList from "./invoiceTable";
+import ProjectList from "./calenderDetail";
 
 const ProtectedRoute = ({ component: Comp, loggedIn, path, ...rest }) => {
   return (
