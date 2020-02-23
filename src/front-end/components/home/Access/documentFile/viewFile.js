116b65d3c3406bfacf07ca8420d807d21e4eb945
ZhangDianxiong <zhangdi@oregonstate.edu>
2020-02-22 19:08:36 -0800

    replace the react favicon by GR faricon

--- src/front-end/components/home/Access/documentFile/viewFile.js
+++ src/front-end/components/home/Access/documentFile/viewFile.js
@@ -5,56 +5,55 @@ import { Grid, Icon, Header, Modal } from "semantic-ui-react";
 import { Route, Link, Redirect, Switch, useParams } from "react-router-dom";
 
 function useProject() {
-	const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-	useEffect(() => {
-		firestore.collection("Document").onSnapshot(snapshot => {
-			const newProject = snapshot.docs.map(doc => ({
-				id: doc.id,
-				...doc.data()
-			}));
-			setProjects(newProject);
-		});
-	}, []);
-	return projects;
+  useEffect(() => {
+    firestore.collection("Document").onSnapshot(snapshot => {
+      const newProject = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setProjects(newProject);
+    });
+  }, []);
+  return projects;
 }
 
 const ProjectList = props => {
-	let { projectid } = useParams();
-	const projects = useProject();
+  let { projectid } = useParams();
+  const projects = useProject();
 
-	return (
-		<div>
-			<Header as="h1">Document</Header>
+  return (
+    <div>
+      <Header as="h1">Document</Header>
 
-			<Helmet>
-				<title>Access</title>
-			</Helmet>
+      <Helmet>
+        <title>Access</title>
+      </Helmet>
 
-			<Grid columns={3} divided>
-				<Grid.Row>
-					{projects.map(project => (
-						<div>
-							<input
-								type="checkbox"
-								onChange={() => props.clickToDelete(project.id)}
-							/>{" "}
-							<Grid.Column>
-								<a
-									key={project.id}
-									href={project.documentLink}
-									data-id={project.id}
-								>
-									<Icon name="file alternate outline" size="massive" />
-								</a>{" "}
-								<label>{project.documentName}</label>
-							</Grid.Column>
-						</div>
-					))}
-				</Grid.Row>
-			</Grid>
-		</div>
-	);
+      <Grid columns={3} divided>
+        <Grid.Row>
+          {projects.map(project => (
+            <div>
+              <Icon name="file alternate outline" size="massive" />
+              <input
+                type="checkbox"
+                onChange={() => props.clickToDelete(project.id)}
+              />{" "}
+              <Grid.Column>
+                <a
+                  key={project.id}
+                  href={project.documentLink}
+                  data-id={project.id}
+                ></a>{" "}
+                <label>{project.documentName}</label>
+              </Grid.Column>
+            </div>
+          ))}
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 };
 
 export default ProjectList;
