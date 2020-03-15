73b834d6e12a56dd9ff3032b258cde847c8ba2cb
ZhangDianxiong <zhangdi@oregonstate.edu>
2020-03-15 12:17:58 -0700

    manage the css of the add/ update button of customer and project page

--- src/front-end/components/Firestore-components/Project/projectDataAdd.js
+++ src/front-end/components/Firestore-components/Project/projectDataAdd.js
@@ -5,189 +5,199 @@ import genUID from "../../../helpers/idGenerator";
 import { Link, Route, withRouter } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import style from "./projectDataAddStyle.css";
 class customerRegistration extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			projectID: "",
-			projectName: "",
-			projectAddress: "",
-			projectType: "",
-			projectStartDate: "",
-			managerID: "",
-			projectDescription: ""
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectID: "",
+      projectName: "",
+      projectAddress: "",
+      projectType: "",
+      projectStartDate: "",
+      managerID: "",
+      projectDescription: ""
+    };
 
-		this.handleChange = this.handleChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-		this.dateHandleChange = this.dateHandleChange.bind(this);
-	}
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.dateHandleChange = this.dateHandleChange.bind(this);
+  }
 
-	onSubmit(e) {
-		e.preventDefault();
-		var tempCustomerID = this.props.match.params.customerid;
-		var email;
+  onSubmit(e) {
+    e.preventDefault();
+    var tempCustomerID = this.props.match.params.customerid;
+    var email;
 
-		firestoreDB
-			.collection("Project")
-			.add({
-				projectID: genUID(),
-				projectName: this.state.projectName,
-				projectAddress: this.state.projectAddress,
-				projectType: this.state.projectType,
-				projectStartDate: this.state.projectStartDate,
-				customerID: this.props.match.params.customerid,
-				managerID: this.state.managerID,
-				projectDescription: this.state.projectDescription
-			})
-			.then(function(docRef) {
-				firestoreDB
-					.collection("Project")
-					.doc(docRef.id)
-					.update({ projectID: docRef.id })
-					.then(test => {
-						// add customerEmail
+    firestoreDB
+      .collection("Project")
+      .add({
+        projectID: genUID(),
+        projectName: this.state.projectName,
+        projectAddress: this.state.projectAddress,
+        projectType: this.state.projectType,
+        projectStartDate: this.state.projectStartDate,
+        customerID: this.props.match.params.customerid,
+        managerID: this.state.managerID,
+        projectDescription: this.state.projectDescription
+      })
+      .then(function(docRef) {
+        firestoreDB
+          .collection("Project")
+          .doc(docRef.id)
+          .update({ projectID: docRef.id })
+          .then(test => {
+            // add customerEmail
 
-						firestoreDB
-							.collection("Customer")
-							.where("customerID", "==", tempCustomerID)
-							.get()
-							.then(querySnapshot => {
-								querySnapshot.forEach(doc => {
-									// doc.data() is never undefined for query doc snapshots
-									email = doc.data().customerEmail;
-								});
+            firestoreDB
+              .collection("Customer")
+              .where("customerID", "==", tempCustomerID)
+              .get()
+              .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                  // doc.data() is never undefined for query doc snapshots
+                  email = doc.data().customerEmail;
+                });
 
-								console.log(docRef.id);
-								//Perform update
-								firestoreDB
-									.collection("Project")
-									.doc(docRef.id)
-									.update({ customerEmail: email })
-									.catch(error => {
-										return this.setState({ status: error });
-									});
-							});
-					})
-					.catch(error => {
-						console.log(error);
-						return this.setState({ status: error });
-					});
-				// console.log("Successfully created: ", docRef.id);
-				// document.getElementById("projectName").value = "";
-				// document.getElementById("projectAddress").value = "";
-				// document.getElementById("projectTypeUnactive").check = false;
-				// document.getElementById("projectTypeActive").check = false;
+                console.log(docRef.id);
+                //Perform update
+                firestoreDB
+                  .collection("Project")
+                  .doc(docRef.id)
+                  .update({ customerEmail: email })
+                  .catch(error => {
+                    return this.setState({ status: error });
+                  });
+              });
+          })
+          .catch(error => {
+            console.log(error);
+            return this.setState({ status: error });
+          });
+        // console.log("Successfully created: ", docRef.id);
+        // document.getElementById("projectName").value = "";
+        // document.getElementById("projectAddress").value = "";
+        // document.getElementById("projectTypeUnactive").check = false;
+        // document.getElementById("projectTypeActive").check = false;
 
-				// document.getElementById("projectStartDate").value = "";
-				// document.getElementById("customerID").value = "";
-				// document.getElementById("managerID").value = "";
-				// document.getElementById("projectDescription").value = "";
-			})
-			.catch(error => {
-				console.log(error);
-				return this.setState({ status: error });
-			});
+        // document.getElementById("projectStartDate").value = "";
+        // document.getElementById("customerID").value = "";
+        // document.getElementById("managerID").value = "";
+        // document.getElementById("projectDescription").value = "";
+      })
+      .catch(error => {
+        console.log(error);
+        return this.setState({ status: error });
+      });
 
-		return this.setState({ status: "Project created Successfully" });
-	}
+    return this.setState({ status: "Project created Successfully" });
+  }
 
-	handleChange(e, { name, value }) {
-		this.setState({ [name]: value });
-	}
+  handleChange(e, { name, value }) {
+    this.setState({ [name]: value });
+  }
 
-	dateHandleChange = date => {
-		// const valueOfInput = this.state.date  <--- I want string with date here
-		console.log("this.state.date", this.state.projectStartDate);
-		this.setState({ projectStartDate: date });
-	};
+  dateHandleChange = date => {
+    // const valueOfInput = this.state.date  <--- I want string with date here
+    console.log("this.state.date", this.state.projectStartDate);
+    this.setState({ projectStartDate: date });
+  };
 
-	render() {
-		const { error } = this.state;
-		return (
-			<div>
-				{/* <navbar /> */}
-				<Grid.Column width={6} />
-				<Grid.Column width={4}>
-					<Form error={error} onSubmit={this.onSubmit}>
-						<Header as="h1">Create Project </Header>
-						{this.state.status && (
-							<Message error={error} content={this.state.status.message} />
-						)}
-						{this.state.status && <Message content={this.state.status} />}
-						<Form.Input
-							inline
-							label="Project Name"
-							type="projectName"
-							id="projectName"
-							name="projectName"
-							placeholder="Project Name..."
-							onChange={this.handleChange}
-						/>
-						<Form.Input
-							inline
-							label="Project Address"
-							type="projectAddress"
-							id="projectAddress"
-							name="projectAddress"
-							placeholder="Project Address..."
-							onChange={this.handleChange}
-						/>
-						<Form.Field>
-							<label>
-								Project Start Date
-								<DatePicker
-									selected={this.state.projectStartDate}
-									onChange={this.dateHandleChange}
-									showTimeSelect
-									timeFormat="HH:mm"
-									timeIntervals={15}
-									timeCaption="time"
-									dateFormat="MMMM d, yyyy h:mm aa"
-								/>
-							</label>
-						</Form.Field>
-						<Form.Input
-							inline
-							label="Project Description"
-							type="projectDescription"
-							id="projectDescription"
-							name="projectDescription"
-							placeholder="projectDescription"
-							onChange={this.handleChange}
-						/>
-						<Form.Input
-							inline
-							label="Manager ID"
-							name="managerID"
-							id="managerID"
-							placeholder="Manager ID..."
-							onChange={this.handleChange}
-						/>{" "}
-						<Form.Group inline>
-							<label>Project Type</label>
-							<Form.Radio
-								label="Active"
-								name="projectType"
-								id="projectType"
-								value="active"
-								onChange={this.handleChange}
-							/>
-							<Form.Radio
-								label="Unactive"
-								name="projectType"
-								id="projectType"
-								value="unactive"
-								onChange={this.handleChange}
-							/>
-						</Form.Group>{" "}
-						<Form.Button type="submit">Create!</Form.Button>
-					</Form>
-				</Grid.Column>
-			</div>
-		);
-	}
+  render() {
+    const { error } = this.state;
+    return (
+      <div>
+        {/* <navbar /> */}
+        <Grid.Column width={6} />
+        <Grid.Column width={4}>
+          <Form error={error} onSubmit={this.onSubmit}>
+            <Header as="h1">Create Project </Header>
+            {this.state.status && (
+              <Message error={error} content={this.state.status.message} />
+            )}
+            {this.state.status && <Message content={this.state.status} />}
+            <Form.Input
+              inline
+              className="projectDataAddField"
+              label="Project Name"
+              type="projectName"
+              id="projectName"
+              name="projectName"
+              placeholder="Project Name..."
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              inline
+              className="projectDataAddField"
+              label="Project Address"
+              type="projectAddress"
+              id="projectAddress"
+              name="projectAddress"
+              placeholder="Project Address..."
+              onChange={this.handleChange}
+            />
+            <Form.Field inline className="projectDataAddField">
+              <label>
+                Project Start Date
+                <DatePicker
+                  selected={this.state.projectStartDate}
+                  onChange={this.dateHandleChange}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={15}
+                  timeCaption="time"
+                  dateFormat="MMMM d, yyyy h:mm aa"
+                />
+              </label>
+            </Form.Field>
+            <Form.Input
+              inline
+              className="projectDataAddField"
+              label="Project Description"
+              type="projectDescription"
+              id="projectDescription"
+              name="projectDescription"
+              placeholder="projectDescription"
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              inline
+              className="projectDataAddField"
+              label="Manager ID"
+              name="managerID"
+              id="managerID"
+              placeholder="Manager ID..."
+              onChange={this.handleChange}
+            />{" "}
+            <Form.Group inline>
+              <label>Project Type</label>
+              <Form.Radio
+                label="Active"
+                name="projectType"
+                id="projectType"
+                value="active"
+                onChange={this.handleChange}
+              />
+              <Form.Radio
+                label="Unactive"
+                name="projectType"
+                id="projectType"
+                value="unactive"
+                onChange={this.handleChange}
+              />
+            </Form.Group>{" "}
+            <Form.Button
+              type="submit"
+              inline
+              className="projectAddConfirmButton"
+            >
+              Create!
+            </Form.Button>
+          </Form>
+        </Grid.Column>
+      </div>
+    );
+  }
 }
 
 export default withRouter(customerRegistration);
--- src/front-end/components/Firestore-components/Project/projectDataEdit.js
+++ src/front-end/components/Firestore-components/Project/projectDataEdit.js
@@ -8,166 +8,177 @@ import { Link, Route, useParams } from "react-router-dom";
 import { SpellInput } from "../../home/CustomerAction/editDeleteCustomer";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import style from "./projectDataEditStyle.css";
 
 var id, dateToUpdate;
 
 function useProject() {
-	const [projects, setProjects] = useState([]);
-	let { projectid } = useParams();
-	let { customerid } = useParams();
-	id = customerid;
+  const [projects, setProjects] = useState([]);
+  let { projectid } = useParams();
+  let { customerid } = useParams();
+  id = customerid;
 
-	let citiesRef = firestore.collection("Project").doc(projectid);
-	var data = new Array();
+  let citiesRef = firestore.collection("Project").doc(projectid);
+  var data = new Array();
 
-	citiesRef
-		.get()
-		.then(doc => {
-			if (!doc.exists) {
-				console.log("No such document!");
-			} else {
-				document.getElementById("userName").value = doc.data().projectName;
-				document.getElementById("email").value = doc
-					.data()
-					.projectStartDate.toDate()
-					.toDateString();
-				document.getElementById(
-					"firstName"
-				).value = doc.data().projectDescription;
-				document.getElementById("lastName").value = doc.data().projectAddress;
-				document.getElementById("phoneNumber").value = doc.data().managerID;
-				document.getElementById("address").value = doc.data().customerID;
-				if (doc.data().projectType === "active") {
-					document.getElementById("customerTypeActive").checked = true;
-				} else {
-					document.getElementById("customerTypeUnactive").checked = true;
-				}
-			}
-		})
-		.catch(err => {
-			console.log("Error getting document", err);
-		});
+  citiesRef
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        document.getElementById("userName").value = doc.data().projectName;
+        document.getElementById("email").value = doc
+          .data()
+          .projectStartDate.toDate()
+          .toDateString();
+        document.getElementById(
+          "firstName"
+        ).value = doc.data().projectDescription;
+        document.getElementById("lastName").value = doc.data().projectAddress;
+        document.getElementById("phoneNumber").value = doc.data().managerID;
+        document.getElementById("address").value = doc.data().customerID;
+        if (doc.data().projectType === "active") {
+          document.getElementById("customerTypeActive").checked = true;
+        } else {
+          document.getElementById("customerTypeUnactive").checked = true;
+        }
+      }
+    })
+    .catch(err => {
+      console.log("Error getting document", err);
+    });
 }
 
 function onSubmit(e) {
-	var radioValue;
-	if (document.getElementById("customerTypeActive").checked) {
-		radioValue = "active";
-	} else {
-		radioValue = "unactive";
-	}
-	console.log(id);
-	e.preventDefault();
-	firestore
-		.collection("Customer")
-		.doc(id)
-		.update({
-			customerEmail: document.getElementById("email").value,
-			customerLastName: document.getElementById("lastName").value,
-			customerFirstName: document.getElementById("firstName").value,
-			customerPhoneNumber: document.getElementById("phoneNumber").value,
-			customerAddress: document.getElementById("address").value,
-			customerUsername: document.getElementById("userName").value,
-			customerType: radioValue
-		});
+  var radioValue;
+  if (document.getElementById("customerTypeActive").checked) {
+    radioValue = "active";
+  } else {
+    radioValue = "unactive";
+  }
+  console.log(id);
+  e.preventDefault();
+  firestore
+    .collection("Customer")
+    .doc(id)
+    .update({
+      customerEmail: document.getElementById("email").value,
+      customerLastName: document.getElementById("lastName").value,
+      customerFirstName: document.getElementById("firstName").value,
+      customerPhoneNumber: document.getElementById("phoneNumber").value,
+      customerAddress: document.getElementById("address").value,
+      customerUsername: document.getElementById("userName").value,
+      customerType: radioValue
+    });
 
-	console.log("Successfully created: ");
+  console.log("Successfully created: ");
 }
 
 const ProjectList = () => {
-	const projects = useProject();
+  const projects = useProject();
 
-	return (
-		<Modal open dimmer="blurring">
-			<Modal.Header>Update Project</Modal.Header>
-			<Modal.Description>
-				<Page title="Update Project">
-					<Helmet>
-						<title>Update Project</title>
-					</Helmet>
-					<Grid.Column width={6} />
-					<Grid.Column width={4}>
-						<Form>
-							<Form.Input
-								inline
-								label="Project Name"
-								type="userName"
-								id="userName"
-								name="userName"
-								placeholder="Username..."
-							/>
-							<Form.Field>
-								<label>
-									Project Start Date: (Note: Cannot Update)
-									<DatePicker
-										id="email"
-										showTimeSelect
-										timeFormat="HH:mm"
-										timeIntervals={15}
-										timeCaption="time"
-										dateFormat="MMMM d, yyyy h:mm aa"
-									/>
-								</label>
-							</Form.Field>
+  return (
+    <Modal open dimmer="blurring">
+      <Modal.Header>Update Project</Modal.Header>
+      <Modal.Description>
+        <Page title="Update Project">
+          <Helmet>
+            <title>Update Project</title>
+          </Helmet>
+          <Grid.Column width={6} />
+          <Grid.Column width={4}>
+            <Form>
+              <Form.Input
+                inline
+                className="projectEditField"
+                label="Project Name"
+                type="userName"
+                id="userName"
+                name="userName"
+                placeholder="Username..."
+              />
+              <Form.Field inline className="timepicker">
+                <label>
+                  Project Start Date: (Note: Cannot Update)
+                  <DatePicker
+                    id="email"
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    timeCaption="time"
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                  />
+                </label>
+              </Form.Field>
 
-							<Form.Input
-								inline
-								label="Project Description"
-								type="firstName"
-								id="firstName"
-								name="firstName"
-								placeholder="First Name..."
-							/>
-							<Form.Input
-								inline
-								label="Project Address"
-								name="lastName"
-								id="lastName"
-								placeholder="Last Name..."
-							/>
-							<Form.Input
-								inline
-								label="Manager ID"
-								name="phoneNumber"
-								id="phoneNumber"
-								placeholder="e.g. 1231234599"
-							/>
-							<Form.Input
-								inline
-								label="Customer ID"
-								name="address"
-								id="address"
-								placeholder="address..."
-							/>
-							<Form.Group inline>
-								<label>Project Type</label>
-								<Form.Radio
-									label="Active"
-									name="customerType"
-									id="customerTypeActive"
-									value="active"
-								/>
-								<Form.Radio
-									label="Unactive"
-									name="customerType"
-									id="customerTypeUnactive"
-									value="unactive"
-								/>
-							</Form.Group>
-							<Form.Button type="submit" onClick={onSubmit}>
-								Update!
-							</Form.Button>
-						</Form>
-					</Grid.Column>
-				</Page>
-			</Modal.Description>
-			<Modal.Actions>
-				<Link to={"/home/" + id + "/project"}>
-					<Button>Close</Button>
-				</Link>
-			</Modal.Actions>
-		</Modal>
-	);
+              <Form.Input
+                inline
+                className="projectEditField"
+                label="Project Description"
+                type="firstName"
+                id="firstName"
+                name="firstName"
+                placeholder="First Name..."
+              />
+              <Form.Input
+                inline
+                className="projectEditField"
+                label="Project Address"
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name..."
+              />
+              <Form.Input
+                inline
+                className="projectEditField"
+                label="Manager ID"
+                name="phoneNumber"
+                id="phoneNumber"
+                placeholder="e.g. 1231234599"
+              />
+              <Form.Input
+                inline
+                className="projectEditField"
+                label="Customer ID"
+                name="address"
+                id="address"
+                placeholder="address..."
+              />
+              <Form.Group inline>
+                <label>Project Type</label>
+                <Form.Radio
+                  label="Active"
+                  name="customerType"
+                  id="customerTypeActive"
+                  value="active"
+                />
+                <Form.Radio
+                  label="Unactive"
+                  name="customerType"
+                  id="customerTypeUnactive"
+                  value="unactive"
+                />
+              </Form.Group>
+              <Form.Button
+                inline
+                type="submit"
+                onClick={onSubmit}
+                className="projectEditConfirmButton"
+              >
+                Update!
+              </Form.Button>
+            </Form>
+          </Grid.Column>
+        </Page>
+      </Modal.Description>
+      <Modal.Actions>
+        <Link to={"/home/" + id + "/project"}>
+          <Button>Close</Button>
+        </Link>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
 export default ProjectList;
