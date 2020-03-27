8c89184101ebba11afcdf2d4759a53517948f1b8
flazzing <limchengqing@gmail.com>
2020-03-27 13:38:21 -0700

    Removed API key

--- src/front-end/config/Fire.js
+++ src/front-end/config/Fire.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-	apiKey: "AIzaSyDtOMz5_Abnd8Et1EHB16oAb_KCgd5x0F0",
-	authDomain: "rogue-model.firebaseapp.com",
-	databaseURL: "https://rogue-model.firebaseio.com",
-	projectId: "rogue-model",
-	storageBucket: "rogue-model.appspot.com",
-	messagingSenderId: "804059970146",
-	appId: "1:804059970146:web:0c68c921b52bdd56552f3b",
-	measurementId: "G-CBBZ1CQJF4"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 
 const fire = firebase.initializeApp(config);
