c3f59434384f2fc7329a49b49ad0e4f224246c2d
flazzing <limchengqing@gmail.com>
2020-04-02 22:40:25 -0700

    fixed the update project unactive problem

--- src/front-end/components/Firestore-components/Project/projectDataEdit.js
+++ src/front-end/components/Firestore-components/Project/projectDataEdit.js
@@ -14,118 +14,118 @@ import Geocode from "react-geocode";
 var id, dateToUpdate, id2;
 
 function useProject() {
-  const [projects, setProjects] = useState([]);
-  let { projectid } = useParams();
-  let { customerid } = useParams();
-  id = projectid;
-  id2 = customerid;
-  let citiesRef = firestore.collection("Project").doc(projectid);
+	const [projects, setProjects] = useState([]);
+	let { projectid } = useParams();
+	let { customerid } = useParams();
+	id = projectid;
+	id2 = customerid;
+	let citiesRef = firestore.collection("Project").doc(projectid);
 
-  var data = new Array();
+	var data = new Array();
 
-  citiesRef
-    .get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log("No such document!");
-      } else {
-        document.getElementById("projectName").value = doc.data().projectName;
-        // document.getElementById("timeDatePicture").value = doc
-        // 	.data()
-        // 	.projectStartDate.toDate()
-        // 	.toDateString();
-        document.getElementById(
-          "projectDescription"
-        ).value = doc.data().projectDescription;
-        document.getElementById(
-          "projectAddress"
-        ).value = doc.data().projectAddress;
-        if (doc.data().projectType === "active") {
-          document.getElementById("projectTypeActive").checked = true;
-        } else {
-          document.getElementById("projectTypeActive").checked = true;
-        }
-      }
-    })
-    .catch(err => {
-      console.log("Error getting document", err);
-    });
+	citiesRef
+		.get()
+		.then(doc => {
+			if (!doc.exists) {
+				console.log("No such document!");
+			} else {
+				document.getElementById("projectName").value = doc.data().projectName;
+				// document.getElementById("timeDatePicture").value = doc
+				// 	.data()
+				// 	.projectStartDate.toDate()
+				// 	.toDateString();
+				document.getElementById(
+					"projectDescription"
+				).value = doc.data().projectDescription;
+				document.getElementById(
+					"projectAddress"
+				).value = doc.data().projectAddress;
+				if (doc.data().projectType === "active") {
+					document.getElementById("projectTypeActive").checked = true;
+				} else {
+					document.getElementById("projectTypeUnactive").checked = true;
+				}
+			}
+		})
+		.catch(err => {
+			console.log("Error getting document", err);
+		});
 }
 
 function onSubmit(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  Geocode.setApiKey("AIzaSyAFex0mi6Ezx0l9IJDPcCiXTw-Xsac0xqg");
-  Geocode.setLanguage("en");
+	Geocode.setApiKey("AIzaSyAFex0mi6Ezx0l9IJDPcCiXTw-Xsac0xqg");
+	Geocode.setLanguage("en");
 
-  var radioValue;
-  if (document.getElementById("projectTypeActive").checked) {
-    radioValue = "active";
-  } else {
-    radioValue = "unactive";
-  }
+	var radioValue;
+	if (document.getElementById("projectTypeActive").checked) {
+		radioValue = "active";
+	} else {
+		radioValue = "unactive";
+	}
 
-  Geocode.fromAddress(document.getElementById("projectAddress").value).then(
-    response => {
-      const { lat, lng } = response.results[0].geometry.location;
+	Geocode.fromAddress(document.getElementById("projectAddress").value).then(
+		response => {
+			const { lat, lng } = response.results[0].geometry.location;
 
-      firestore
-        .collection("Project")
-        .doc(id)
-        .update({
-          // customerEmail: document.getElementById("timeDatePicture").value,
-          projectAddress: document.getElementById("projectAddress").value,
-          projectDescription: document.getElementById("projectDescription")
-            .value,
-          projectName: document.getElementById("projectName").value,
-          projectLatitude: lat,
-          projectLongitude: lng,
-          projectType: radioValue
-        })
-        .then(gratz => {})
-        .catch(err => {
-          console.log("error");
-        });
+			firestore
+				.collection("Project")
+				.doc(id)
+				.update({
+					// customerEmail: document.getElementById("timeDatePicture").value,
+					projectAddress: document.getElementById("projectAddress").value,
+					projectDescription: document.getElementById("projectDescription")
+						.value,
+					projectName: document.getElementById("projectName").value,
+					projectLatitude: lat,
+					projectLongitude: lng,
+					projectType: radioValue
+				})
+				.then(gratz => {})
+				.catch(err => {
+					console.log("error");
+				});
 
-      console.log(lat, lng);
-    },
-    error => {
-      alert("Not a valid address. Please try again.");
-    }
-  );
-  console.log("Successfully created: ");
+			console.log(lat, lng);
+		},
+		error => {
+			alert("Not a valid address. Please try again.");
+		}
+	);
+	console.log("Successfully created: ");
 }
 
 const ProjectList = () => {
-  const projects = useProject();
+	const projects = useProject();
 
-  return (
-    <Modal open dimmer="blurring">
-      <div style={{ float: "right" }}>
-        <Link to={"/home/" + id2 + "/project"}>
-          <Icon name="close" size="large" />
-        </Link>
-      </div>
-      <Modal.Header>Update Project</Modal.Header>
+	return (
+		<Modal open dimmer="blurring">
+			<div style={{ float: "right" }}>
+				<Link to={"/home/" + id2 + "/project"}>
+					<Icon name="close" size="large" />
+				</Link>
+			</div>
+			<Modal.Header>Update Project</Modal.Header>
 
-      <Modal.Description>
-        <Page title="Update Project">
-          <Helmet>
-            <title>Update Project</title>
-          </Helmet>
-          <Grid.Column width={6} />
-          <Grid.Column width={4}>
-            <Form>
-              <Form.Input
-                inline
-                className="projectEditField"
-                label="Project Name"
-                type="projectName"
-                id="projectName"
-                name="projectName"
-                placeholder="projectName..."
-              />
-              {/* <Form.Field inline className="timepicker">
+			<Modal.Description>
+				<Page title="Update Project">
+					<Helmet>
+						<title>Update Project</title>
+					</Helmet>
+					<Grid.Column width={6} />
+					<Grid.Column width={4}>
+						<Form>
+							<Form.Input
+								inline
+								className="projectEditField"
+								label="Project Name"
+								type="projectName"
+								id="projectName"
+								name="projectName"
+								placeholder="projectName..."
+							/>
+							{/* <Form.Field inline className="timepicker">
 
 								<label>
 									Project Start Date: (Note: Cannot Update)
@@ -140,58 +140,58 @@ const ProjectList = () => {
 								</label>
 							</Form.Field> */}
 
-              <Form.Input
-                inline
-                className="projectEditField"
-                label="Project Description"
-                type="projectDescription"
-                id="projectDescription"
-                name="projectDescription"
-                placeholder="projectDescription..."
-              />
-              <Form.Input
-                inline
-                className="projectEditField"
-                label="Project Address"
-                name="projectAddress"
-                id="projectAddress"
-                placeholder="projectAddress..."
-              />
+							<Form.Input
+								inline
+								className="projectEditField"
+								label="Project Description"
+								type="projectDescription"
+								id="projectDescription"
+								name="projectDescription"
+								placeholder="projectDescription..."
+							/>
+							<Form.Input
+								inline
+								className="projectEditField"
+								label="Project Address"
+								name="projectAddress"
+								id="projectAddress"
+								placeholder="projectAddress..."
+							/>
 
-              <Form.Group inline>
-                <label>Project Type</label>
-                <Form.Radio
-                  label="Active"
-                  name="projectType"
-                  id="projectTypeActive"
-                  value="active"
-                />
-                <Form.Radio
-                  label="Unactive"
-                  name="projectType"
-                  id="customerTypeUnactive"
-                  value="unactive"
-                />
-              </Form.Group>
-              <Form.Button
-                inline
-                type="submit"
-                onClick={onSubmit}
-                className="projectEditConfirmButton"
-              >
-                Update!
-              </Form.Button>
-            </Form>
-          </Grid.Column>
-        </Page>
-      </Modal.Description>
-      <Modal.Actions>
-        <Link to={"/home/" + id2 + "/project"}>
-          <Button>Close</Button>
-        </Link>
-      </Modal.Actions>
-    </Modal>
-  );
+							<Form.Group inline>
+								<label>Project Type</label>
+								<Form.Radio
+									label="Active"
+									name="projectType"
+									id="projectTypeActive"
+									value="active"
+								/>
+								<Form.Radio
+									label="Unactive"
+									name="projectType"
+									id="projectTypeUnactive"
+									value="unactive"
+								/>
+							</Form.Group>
+							<Form.Button
+								inline
+								type="submit"
+								onClick={onSubmit}
+								className="projectEditConfirmButton"
+							>
+								Update!
+							</Form.Button>
+						</Form>
+					</Grid.Column>
+				</Page>
+			</Modal.Description>
+			<Modal.Actions>
+				<Link to={"/home/" + id2 + "/project"}>
+					<Button>Close</Button>
+				</Link>
+			</Modal.Actions>
+		</Modal>
+	);
 };
 
 export default ProjectList;
