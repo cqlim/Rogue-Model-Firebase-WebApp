0e8ef4fd3cd8bb55c60c8fada784eb7710e1cb39
ZhangDianxiong <zhangdi@oregonstate.edu>
2020-04-06 10:21:42 -0700

    changed taske placed date to due days, enable the  type selection on customer, project, task

--- src/front-end/components/home/ProjectAction/addProjectModal.js
+++ src/front-end/components/home/ProjectAction/addProjectModal.js
@@ -3,57 +3,57 @@ import firestore from "../../../config/firestore";
 import { Table, Menu, Modal, Button, Icon } from "semantic-ui-react";
 
 import {
-	useParams,
-	Link,
-	Redirect,
-	Switch,
-	BrowserRouter
+  useParams,
+  Link,
+  Redirect,
+  Switch,
+  BrowserRouter
 } from "react-router-dom";
 import AddProject from "../../Firestore-components/Project/projectDataAdd";
 
 var id;
 
 function useProject() {
-	const [projects, setProjects] = useState([]);
-	let { customerid } = useParams();
-	id = customerid;
+  const [projects, setProjects] = useState([]);
+  let { customerid } = useParams();
+  id = customerid;
 
-	useEffect(() => {
-		firestore.collection("Customer").onSnapshot(snapshot => {
-			const newProject = snapshot.docs.map(doc => ({
-				id: doc.id,
-				...doc.data()
-			}));
-			setProjects(newProject);
-		});
-	}, []);
-	return projects;
+  useEffect(() => {
+    firestore.collection("Customer").onSnapshot(snapshot => {
+      const newProject = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setProjects(newProject);
+    });
+  }, []);
+  return projects;
 }
 
 const ProjectList = () => {
-	const projects = useProject();
-	let { customerid } = useParams();
+  const projects = useProject();
+  let { customerid } = useParams();
 
-	return (
-		<div>
-			<Modal open dimmer="blurring">
-				<div style={{ float: "right" }}>
-					<Link to={"/home/" + id + "/project"}>
-						<Icon name="close" size="large" />
-					</Link>
-				</div>
-				<Modal.Header>Add Project</Modal.Header>
-				<Modal.Description>{<AddProject />}</Modal.Description>
-				<Modal.Actions>
-					<Link to={"/home/" + customerid + "/project"}>
-						<Button>Close</Button>
-					</Link>
-				</Modal.Actions>
-			</Modal>
+  return (
+    <div>
+      <Modal open dimmer="blurring">
+        <div style={{ float: "right" }}>
+          <Link to={"/home/" + id + "/project"}>
+            <Icon name="close" size="large" />
+          </Link>
+        </div>
+        <Modal.Header>Add Project</Modal.Header>
+        <Modal.Description>{<AddProject />}</Modal.Description>
+        <Modal.Actions>
+          <Link to={"/home/" + customerid + "/project"}>
+            <Button>Close</Button>
+          </Link>
+        </Modal.Actions>
+      </Modal>
 
-			{/* </Page> */}
-		</div>
-	);
+      {/* </Page> */}
+    </div>
+  );
 };
 
 export default ProjectList;
