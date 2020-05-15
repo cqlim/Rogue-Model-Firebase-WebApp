669c04d30d2f24b25fa6c7c6a784a3ae8a83d7ee
ZhangDianxiong <zhangdi@oregonstate.edu>
2020-05-15 16:22:29 -0700

    change the enumerate types of document, fix the radio button issue in account create page

--- src/front-end/components/Firestore-components/Customer/customerDataEdit.js
+++ src/front-end/components/Firestore-components/Customer/customerDataEdit.js
@@ -10,170 +10,171 @@ import style from "./style.css";
 var id;
 
 function useProject() {
-	const [projects, setProjects] = useState([]);
-	let { customerid } = useParams();
-	id = customerid;
+  const [projects, setProjects] = useState([]);
+  let { customerid } = useParams();
+  id = customerid;
 
-	let citiesRef = firestore.collection("Customer").doc(customerid);
-	var data = new Array();
-	citiesRef
-		.get()
-		.then(doc => {
-			if (!doc.exists) {
-				console.log("No such document!");
-			} else {
-				document.getElementById("userName").value = doc.data().customerUsername;
-				document.getElementById("email").value = doc.data().customerEmail;
-				document.getElementById(
-					"firstName"
-				).value = doc.data().customerFirstName;
-				document.getElementById("lastName").value = doc.data().customerLastName;
-				document.getElementById(
-					"phoneNumber"
-				).value = doc.data().customerPhoneNumber;
-				document.getElementById("address").value = doc.data().customerAddress;
-				if (doc.data().customerType === "active") {
-					document.getElementById("customerTypeActive").checked = true;
-				} else {
-					document.getElementById("customerTypeUnactive").checked = true;
-				}
-			}
-		})
-		.catch(err => {
-			console.log("Error getting document", err);
-		});
+  let citiesRef = firestore.collection("Customer").doc(customerid);
+  var data = new Array();
+  citiesRef
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        document.getElementById("userName").value = doc.data().customerUsername;
+        document.getElementById("email").value = doc.data().customerEmail;
+        document.getElementById(
+          "firstName"
+        ).value = doc.data().customerFirstName;
+        document.getElementById("lastName").value = doc.data().customerLastName;
+        document.getElementById(
+          "phoneNumber"
+        ).value = doc.data().customerPhoneNumber;
+        document.getElementById("address").value = doc.data().customerAddress;
+        if (doc.data().customerType === "active") {
+          document.getElementById("customerTypeActive").checked = true;
+        } else {
+          document.getElementById("customerTypeUnactive").checked = true;
+        }
+      }
+    })
+    .catch((err) => {
+      console.log("Error getting document", err);
+    });
 }
 
 function onSubmit(e) {
-	var radioValue;
-	if (document.getElementById("customerTypeActive").checked) {
-		radioValue = "active";
-	} else {
-		radioValue = "unactive";
-	}
-	console.log(id);
-	e.preventDefault();
-	firestore
-		.collection("Customer")
-		.doc(id)
-		.update({
-			customerEmail: document.getElementById("email").value,
-			customerLastName: document.getElementById("lastName").value,
-			customerFirstName: document.getElementById("firstName").value,
-			customerPhoneNumber: document.getElementById("phoneNumber").value,
-			customerAddress: document.getElementById("address").value,
-			customerUsername: document.getElementById("userName").value,
-			customerType: radioValue
-		});
-
-	console.log("Successfully created: ");
+  var radioValue;
+  if (document.getElementById("customerTypeActive").checked) {
+    radioValue = "active";
+  } else {
+    radioValue = "unactive";
+  }
+  console.log(id);
+  e.preventDefault();
+  firestore
+    .collection("Customer")
+    .doc(id)
+    .update({
+      customerEmail: document.getElementById("email").value,
+      customerLastName: document.getElementById("lastName").value,
+      customerFirstName: document.getElementById("firstName").value,
+      customerPhoneNumber: document.getElementById("phoneNumber").value,
+      customerAddress: document.getElementById("address").value,
+      customerUsername: document.getElementById("userName").value,
+      customerType: radioValue,
+    })
+    .catch((err) => {
+      console.err(err);
+    });
 }
 
 const ProjectList = () => {
-	const projects = useProject();
-	console.log(projects + "Outer");
-	return (
-		<Modal open dimmer="blurring">
-			<div style={{ float: "right" }}>
-				<Link to="/home">
-					<Icon name="close" size="large" />
-				</Link>
-			</div>
+  const projects = useProject();
+  console.log(projects + "Outer");
+  return (
+    <Modal open dimmer="blurring">
+      <div style={{ float: "right" }}>
+        <Link to="/home">
+          <Icon name="close" size="large" />
+        </Link>
+      </div>
 
-			<Modal.Header>Update Customer</Modal.Header>
-			<Modal.Description>
-				<Page title="Update Customer">
-					<Helmet>
-						<title>Update Customer</title>
-					</Helmet>
-					<Grid.Column width={6} />
-					<Grid.Column width={4}>
-						<Form>
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="Username"
-								type="userName"
-								id="userName"
-								name="userName"
-								placeholder="Username..."
-							/>
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="Email"
-								type="email"
-								id="email"
-								name="email"
-								placeholder="Email..."
-							/>
+      <Modal.Header>Update Customer</Modal.Header>
+      <Modal.Description>
+        <Page title="Update Customer">
+          <Helmet>
+            <title>Update Customer</title>
+          </Helmet>
+          <Grid.Column width={6} />
+          <Grid.Column width={4}>
+            <Form>
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="Username"
+                type="userName"
+                id="userName"
+                name="userName"
+                placeholder="Username..."
+              />
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="Email"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email..."
+              />
 
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="First Name"
-								type="firstName"
-								id="firstName"
-								name="firstName"
-								placeholder="First Name..."
-							/>
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="Last Name"
-								name="lastName"
-								id="lastName"
-								placeholder="Last Name..."
-							/>
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="Phone Number"
-								name="phoneNumber"
-								id="phoneNumber"
-								placeholder="e.g. 1231234599"
-							/>
-							<Form.Input
-								className="customerEditing"
-								inline
-								label="Address"
-								name="address"
-								id="address"
-								placeholder="address..."
-							/>
-							<Form.Group inline>
-								<label>Customer Type</label>
-								<Form.Radio
-									label="Active"
-									name="customerType"
-									id="customerTypeActive"
-									value="active"
-								/>
-								<Form.Radio
-									label="Unactive"
-									name="customerType"
-									id="customerTypeUnactive"
-									value="unactive"
-								/>
-							</Form.Group>
-							<Form.Button
-								type="submit"
-								onClick={onSubmit}
-								className="cutomerEditConfirm"
-							>
-								Update!
-							</Form.Button>
-						</Form>
-					</Grid.Column>
-				</Page>
-			</Modal.Description>
-			<Modal.Actions>
-				<Link to="/home">
-					<Button>Close</Button>
-				</Link>
-			</Modal.Actions>
-		</Modal>
-	);
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="First Name"
+                type="firstName"
+                id="firstName"
+                name="firstName"
+                placeholder="First Name..."
+              />
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="Last Name"
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name..."
+              />
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="Phone Number"
+                name="phoneNumber"
+                id="phoneNumber"
+                placeholder="e.g. 1231234599"
+              />
+              <Form.Input
+                className="customerEditing"
+                inline
+                label="Address"
+                name="address"
+                id="address"
+                placeholder="address..."
+              />
+              <Form.Group inline>
+                <label>Customer Type</label>
+                <Form.Radio
+                  label="Active"
+                  name="customerType"
+                  id="customerTypeActive"
+                  value="active"
+                />
+                <Form.Radio
+                  label="Unactive"
+                  name="customerType"
+                  id="customerTypeUnactive"
+                  value="unactive"
+                />
+              </Form.Group>
+              <Form.Button
+                type="submit"
+                onClick={onSubmit}
+                className="cutomerEditConfirm"
+              >
+                Update!
+              </Form.Button>
+            </Form>
+          </Grid.Column>
+        </Page>
+      </Modal.Description>
+      <Modal.Actions>
+        <Link to="/home">
+          <Button>Close</Button>
+        </Link>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
 export default ProjectList;
